5729589a974999cd943913445fd9699169b8fec1
byzxc <59598406+byzxc@users.noreply.github.com>
2022-05-31 10:48:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> Simon.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/TeamWDK/WDK_Engine"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn GAM300
-        </a>
-      </header>
-    </div>
-  );
-}
-
 class Square extends React.Component
 {
   render()
