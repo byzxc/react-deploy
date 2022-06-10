bd7736c640a8a6c71f51b122a60c3337feed12f1
byzxc <59598406+byzxc@users.noreply.github.com>
2022-06-10 20:18:20 +0800

    Trying to implement Simple Line chart

--- src/App.js
+++ src/App.js
@@ -1,90 +1,167 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> Simon.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/TeamWDK/WDK_Engine"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn GAM300
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello <code>src/App.js</code> Simon.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://github.com/TeamWDK/WDK_Engine"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn GAM300
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-class Square extends React.Component
-{
-  render()
+// Data
+const data = [
   {
-    return (
-    <button className="square">
-      {this.props.value}
-    </button>
-    );
-  }
-}
-
-class Board extends React.Component
-{
-  renderSquare(i)
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
   {
-    // pass a propertie called "value" to Square
-    return <Square value={i} />;
-  }
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+// class Square extends React.Component
+// {
+//   render()
+//   {
+//     return (
+//     <button className="square">
+//       {this.props.value}
+//     </button>
+//     );
+//   }
+// }
+
+// class Board extends React.Component
+// {
+//   renderSquare(i)
+//   {
+//     // pass a propertie called "value" to Square
+//     return <Square value={i} />;
+//   }
+
+//   render() {
+//     const status = 'Next player: X';
+
+//     return (
+//       <div>
+//         <div className="status">{status}</div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// class Game extends React.Component
+// {
+//   render()
+//   {
+//       return (
+//         <div className="game">
+//           <div className="game-board">
+//             <Board />
+//           </div>
+//           <div className="game-info">
+//             <div>{/* status */}</div>
+//             <ol>{/* TODO */}</ol>
+//           </div>
+//         </div>
+//       );
+//   }
+// }
+
+// export default Game;
+
+export default class Example extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
 
   render() {
-    const status = 'Next player: X';
-
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     );
   }
 }
-
-class Game extends React.Component
-{
-  render()
-  {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-  }
-}
-
-export default Game;
