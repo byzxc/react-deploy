ee8ce3f7ccc36834148bd67bd6e6a320c34ef42b
byzxc <59598406+byzxc@users.noreply.github.com>
2022-06-21 21:59:44 +0800

    - Added DeclareWinner function - Added boolean xIsNext to help in toggle between O and X - Change Square class into function where inside the function it will call the onClick function that will call the handleClick function from Board

--- src/App.js
+++ src/App.js
@@ -1,56 +1,103 @@
 import './App.css';
 import React from 'react';
 
-class Square extends React.Component
+// Function to declaring a winner
+function calculateWinner(squares)
 {
-  // Use constructor to take in propertie that stores the state of the square
-  constructor(props)
+  // Ways to win the 3x3 tic tac toe
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; ++i)
   {
-    // In javscript classes, you need to always call super when defining the constructor 
-    // of a subclass
-    super(props);
-    this.state = {
-      value: null,
-    };
+    // 1 row, 3 cols
+    const [a,b,c] = lines[i];
+    // As long as there is one diagonal or straight line same, the player wins.
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+    {
+      return squares[a];
+    }
   }
-  
-  
-  render()
-  {
-    // React will only call this function after a click
-    // It will set the State of the square to X
-    // You will see X on the square
-    // setState will automatically updates the child component inside of it
-    return (
-      <button className="square" 
-        onClick={() => this.setState({value: 'X'})}
-      >
-      {this.state.value}
+  return null;
+}
+
+// React will only call this function after a click
+// It will set the State of the square to X
+// You will see X on the square
+// setState will automatically updates the child component inside of it
+function Square(props)
+{
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
     </button>
-    );
-  }
+  );
 }
 
 class Board extends React.Component
 {
   // Create a constructor to create the board
   // of an array of 9 squares to be null
+  // Default on the square is X by using the boolean xIsNext to see if to flip X or O
   constructor(props)
   {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
-  
+
+  // Define handeClick function
+  handleClick(i)
+  {
+    // Returns index 0 of the array if no number is passed in to slice();
+    // .slice() create a copy of the squares
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    // This is essentially this((prevState) => ({stateName: prevState.stateName + 1}))
+    // This will allow to me toggle between O and X
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i)
   {
-    // pass a propertie called "value" to Square
-    return <Square value={this.state.squares[i]} />;
+    // Pass down a function from Board to Square,
+    // so that the function will be called by Square when a square is clicked
+    // Will call function Square that takes in argument of props
+    return (
+        <Square 
+          value={this.state.squares[i]}
+          // Square will call onClick which will call Board.handleClick(i)
+          onClick={() => this.handleClick(i)}
+        />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner)
+    {
+        status = 'Winner: ' + winner;
+    }
+    else
+    {
+      // Depending on the state of xIsNext, we display X or O
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
 
     return (
       <div>
